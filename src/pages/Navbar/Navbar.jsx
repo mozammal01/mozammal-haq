ca1fd74a5c84a4950fe2052797ab18b18c80c4df
Mozammal Haq <mozammalhaq01@gmail.com>
2024-12-17 12:09:33 +0600

    navbar updated

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -1,17 +1,88 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink} from "react-router-dom";
+import logo from '../../assets/logo.png'
 
 const Navbar = () => {
+  // const location = useLocation();
+
+  // useEffect(() => {
+
+  //   console.log(location);
+  //   const home = location.pathname === '/'
+
+  //   try {
+  //     if (location.pathname) {
+  //       if (home) {
+  //         let navHome = document.getElementById("home")
+  //         if (navHome) {
+  //           navHome.className = 'bg-blue-500'
+  //         }
+  //         else {
+  //           console.log('fuck You');
+  //         }
+  //         console.log(navHome);
+  //       }
+  //     }
+  //   }
+  //   catch (err) {
+  //     console.log(err);
+  //   }
+  // }, [location])
   const navLinks = [
     <>
-      <Link to='/'><li><a>Home</a></li></Link>
-      <Link to='/about'><li><a>About</a></li></Link>
-      <Link to='/services'><li><a>Services</a></li></Link>
-      <Link to='/pages'><li><a>Pages</a></li></Link>
-      <Link to='/contact'><li><a>Contact</a></li></Link>
+    
+      {/* Home */}
+      <li><NavLink
+        to="/"
+        style={({ isActive }) => {
+          return isActive ? { color: "white" } : {};
+        }}
+      >
+        Home
+      </NavLink></li>
+      
+      {/* About */}
+      <li><NavLink
+        to="/about"
+        style={({ isActive }) => {
+          return isActive ? { color: "plum" } : {};
+        }}
+      >
+        About
+      </NavLink></li>
+      
+      {/* Pages */}
+      <li><NavLink
+        to="/pages"
+        style={({ isActive }) => {
+          return isActive ? { color: "plum" } : {};
+        }}
+      >
+        Pages
+      </NavLink></li>
+
+      {/* services */}
+      <li><NavLink
+        to="/services"
+        style={({ isActive }) => {
+          return isActive ? { color: "plum" } : {};
+        }}
+      >
+        Services
+      </NavLink></li>
+
+      {/* Contact */}
+      <li><NavLink
+        to="/contact"
+        style={({ isActive }) => {
+          return isActive ? { color: "plum" } : {};
+        }}
+      >
+        Contact
+      </NavLink></li>
     </>
   ]
   return (
-    <div className="navbar bg-base-100 h-20">
+    <div className="navbar bg-base-100 h-16">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -34,15 +105,15 @@ const Navbar = () => {
             {navLinks}
           </ul>
         </div>
-        <a className="btn btn-ghost text-2xl">MH</a>
+        <Link to='/'><a className="ms-10"><img width={70} src={logo} alt="logo" /></a></Link>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1 font-bold text-lg">
-        {navLinks}
+        <ul className="menu menu-horizontal px-1 font-bold text-lg gap-7">
+          {navLinks}
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">My Resume</a>
+        <a className="btn font-bold">My Resume</a>
       </div>
     </div>
   );
