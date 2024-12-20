deb062908992f5974495cccf82371c949464a887
Mozammal Haq <mozammalhaq01@gmail.com>
2024-12-21 00:15:36 +0600

    banner , left side nav, right side nav done

--- src/components/button/UpToDown.jsx
+++ src/components/button/UpToDown.jsx
@@ -1,22 +1,22 @@
 import PropTypes from 'prop-types';
-
 const UpToDown = ({ className, btnText }) => {
+  
   return (
     <>
       {/* Up To Down */}
       <a href="#"
-          className={`${className} relative bg-white rounded-lg overflow-hidden border-2 border-green-400 text-green-400   duration-500 hover:text-white py-2.5 px-5 group`}>
-          <span
-            className="absolute w-64 h-64 mb-12 group-hover:-mb-24 transition-all ease-linear duration-500 bg-green-400 left-0 bottom-0"></span>
-          <span className="relative">{btnText}</span>
-        </a>
+        className={`${className} relative bg-white rounded-lg overflow-hidden border-2 border-green-400 text-green-400 duration-500 hover:text-white py-2.5 px-5 group`}>
+        <span
+          className="absolute w-64 h-64 mb-12 group-hover:-mb-24 transition-all ease-linear duration-500 bg-green-400 left-0 bottom-0"></span>
+        <span className="relative">{btnText}</span>
+      </a>
     </>
   );
 };
 
 UpToDown.propTypes = {
   className: PropTypes.string,
-    btnText: PropTypes.string,
+  btnText: PropTypes.string,
 };
 
 export default UpToDown;
--- src/pages/Navbar/RightSideNav.jsx
+++ src/pages/Navbar/RightSideNav.jsx
@@ -1,7 +1,7 @@
 const RightSideNav = () => {
   return (
     <div className="h-screen fixed w-1 top-72">
-      <div className="rotate-90 font-bold cursor-pointer hover:text-green-400 transition ease-in-out  hover:-translate-y-1 hover:scale-110 duration-200">mozammalhaq01@gmail.com</div>
+      <div className="rotate-90 font-semibold cursor-pointer hover:text-green-400 transition ease-in-out  hover:-translate-y-1 hover:scale-110 duration-200">mozammalhaq01@gmail.com</div>
     </div>
   );
 };
